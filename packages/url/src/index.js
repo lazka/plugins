ae5f4fb76a37154ee4598c7c865a9f1ae2d43b75
Christoph Reiter <reiter.christoph@gmail.com>
2025-10-19 08:49:36 +0200

    wip: port url

--- packages/url/src/index.js
+++ packages/url/src/index.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 
 import makeDir from 'make-dir';
 import mime from 'mime';
-import { createFilter } from '@rollup/pluginutils';
+import { createHookFilter } from '@rollup/pluginutils';
 
 const fsStatPromise = util.promisify(fs.stat);
 const fsReadFilePromise = util.promisify(fs.readFile);
@@ -21,50 +21,51 @@ export default function url(options = {}) {
     emitFiles = true,
     fileName = '[hash][extname]'
   } = options;
-  const filter = createFilter(include, exclude);
 
   const copies = Object.create(null);
 
   return {
     name: 'url',
-    load(id) {
-      if (!filter(id)) {
-        return null;
-      }
-      this.addWatchFile(id);
-      return Promise.all([fsStatPromise(id), fsReadFilePromise(id)]).then(([stats, buffer]) => {
-        let data;
-        if ((limit && stats.size > limit) || limit === 0) {
-          const hash = crypto.createHash('sha1').update(buffer).digest('hex').substr(0, 16);
-          const ext = path.extname(id);
-          const name = path.basename(id, ext);
-          // Determine the directory name of the file based
-          // on either the relative path provided in options,
-          // or the parent directory
-          const relativeDir = options.sourceDir
-            ? path.relative(options.sourceDir, path.dirname(id))
-            : path.dirname(id).split(sep).pop();
+    load: {
+      filter: {
+        id: createHookFilter(include, exclude)
+      },
+      handler(id) {
+        this.addWatchFile(id);
+        return Promise.all([fsStatPromise(id), fsReadFilePromise(id)]).then(([stats, buffer]) => {
+          let data;
+          if ((limit && stats.size > limit) || limit === 0) {
+            const hash = crypto.createHash('sha1').update(buffer).digest('hex').substr(0, 16);
+            const ext = path.extname(id);
+            const name = path.basename(id, ext);
+            // Determine the directory name of the file based
+            // on either the relative path provided in options,
+            // or the parent directory
+            const relativeDir = options.sourceDir
+              ? path.relative(options.sourceDir, path.dirname(id))
+              : path.dirname(id).split(sep).pop();
 
-          // Generate the output file name based on some string
-          // replacement parameters
-          const outputFileName = fileName
-            .replace(/\[hash\]/g, hash)
-            .replace(/\[extname\]/g, ext)
-            // use `sep` for windows environments
-            .replace(/\[dirname\]/g, relativeDir === '' ? '' : `${relativeDir}${sep}`)
-            .replace(/\[name\]/g, name);
-          // Windows fix - exports must be in unix format
-          data = `${publicPath}${outputFileName.split(sep).join(posix.sep)}`;
-          copies[id] = outputFileName;
-        } else {
-          const mimetype = mime.getType(id);
-          const isSVG = mimetype === 'image/svg+xml';
-          data = isSVG ? encodeSVG(buffer) : buffer.toString('base64');
-          const encoding = isSVG ? '' : ';base64';
-          data = `data:${mimetype}${encoding},${data}`;
-        }
-        return `export default "${data}"`;
-      });
+            // Generate the output file name based on some string
+            // replacement parameters
+            const outputFileName = fileName
+              .replace(/\[hash\]/g, hash)
+              .replace(/\[extname\]/g, ext)
+              // use `sep` for windows environments
+              .replace(/\[dirname\]/g, relativeDir === '' ? '' : `${relativeDir}${sep}`)
+              .replace(/\[name\]/g, name);
+            // Windows fix - exports must be in unix format
+            data = `${publicPath}${outputFileName.split(sep).join(posix.sep)}`;
+            copies[id] = outputFileName;
+          } else {
+            const mimetype = mime.getType(id);
+            const isSVG = mimetype === 'image/svg+xml';
+            data = isSVG ? encodeSVG(buffer) : buffer.toString('base64');
+            const encoding = isSVG ? '' : ';base64';
+            data = `data:${mimetype}${encoding},${data}`;
+          }
+          return `export default "${data}"`;
+        });
+      }
     },
     generateBundle: async function write(outputOptions) {
       // Allow skipping saving files for server side builds.
